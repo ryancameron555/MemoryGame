99c818d37d89a65c0d69f712380bd0c6010486b2
Ryancameron555 <147696086+Ryancameron555@users.noreply.github.com>
2025-06-23 17:20:35 +1200

    Adjusted image reference to fix live site issue

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from 'react';
-import Card from './Card';
+import Card from './Card.jsx';
 import '../styles/App.css';
 
 export default function Board({
--- src/data/card.js
+++ src/data/card.js
@@ -1,24 +1,38 @@
 /** @format */
 
+import CLUB1 from '../assets/cards/CLUB-1.svg';
+import CLUB2 from '../assets/cards/CLUB-2.svg';
+import CLUB3 from '../assets/cards/CLUB-3.svg';
+import CLUB4 from '../assets/cards/CLUB-4.svg';
+import CLUB5 from '../assets/cards/CLUB-5.svg';
+import CLUB6 from '../assets/cards/CLUB-6.svg';
+import CLUB7 from '../assets/cards/CLUB-7.svg';
+import CLUB8 from '../assets/cards/CLUB-8.svg';
+import CLUB9 from '../assets/cards/CLUB-9.svg';
+import CLUB10 from '../assets/cards/CLUB-10.svg';
+import CLUBJ from '../assets/cards/CLUB-11-JACK.svg';
+import CLUBQ from '../assets/cards/CLUB-12-QUEEN.svg';
+import CLUBK from '../assets/cards/CLUB-13-KING.svg';
+
 const cardImages = [
-  { name: 'club1', img: '/images/cards/CLUB-1.svg' },
-  { name: 'club2', img: '/images/cards/CLUB-2.svg' },
-  { name: 'club3', img: '/images/cards/CLUB-3.svg' },
-  { name: 'club4', img: '/images/cards/CLUB-4.svg' },
-  { name: 'club5', img: '/images/cards/CLUB-5.svg' },
-  { name: 'club6', img: '/images/cards/CLUB-6.svg' },
-  { name: 'club7', img: '/images/cards/CLUB-7.svg' },
-  { name: 'club8', img: '/images/cards/CLUB-8.svg' },
-  { name: 'club9', img: '/images/cards/CLUB-9.svg' },
-  { name: 'club10', img: '/images/cards/CLUB-10.svg' },
-  { name: 'clubJ', img: '/images/cards/CLUB-11-JACK.svg' },
-  { name: 'clubQ', img: '/images/cards/CLUB-12-QUEEN.svg' },
-  { name: 'clubK', img: '/images/cards/CLUB-13-KING.svg' },
+  { name: 'club1', img: CLUB1 },
+  { name: 'club2', img: CLUB2 },
+  { name: 'club3', img: CLUB3 },
+  { name: 'club4', img: CLUB4 },
+  { name: 'club5', img: CLUB5 },
+  { name: 'club6', img: CLUB6 },
+  { name: 'club7', img: CLUB7 },
+  { name: 'club8', img: CLUB8 },
+  { name: 'club9', img: CLUB9 },
+  { name: 'club10', img: CLUB10 },
+  { name: 'clubJ', img: CLUBJ },
+  { name: 'clubQ', img: CLUBQ },
+  { name: 'clubK', img: CLUBK },
 ];
 
 export function generateShuffledDeck() {
   const deck = [...cardImages, ...cardImages] // duplicate cards for matching
-    .map((card, index) => ({ ...card, id: index + '-' + card.name }))
+    .map((card, index) => ({ ...card, id: `${index}- ${card.name}` }))
     .sort(() => Math.random() - 0.5); // shuffle
 
   return deck;
